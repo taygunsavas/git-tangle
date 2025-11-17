0e55414c3f91769fa88be690029f5a7c66bac51c
Taygun Savaş <taygunsavas@windowslive.com>
2025-11-17 11:34:15 +0300

    chore(ci): :construction_worker: configure git identity for Linux tests

--- scripts/ci-run-tests.sh
+++ scripts/ci-run-tests.sh
@@ -5,4 +5,9 @@ bats --version || true
 git --version
 uname -a
 
+export GIT_AUTHOR_NAME="Git Tangle CI"
+export GIT_AUTHOR_EMAIL="ci@git-tangle.test"
+export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
+
 bats -r tests
